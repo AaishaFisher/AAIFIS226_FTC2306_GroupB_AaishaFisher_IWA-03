754e5f95135d7c99ea638eaf6150d1e8d3e4ed1c
Aai'sha Fisher <82815863+AaishaFisher@users.noreply.github.com>
2023-08-02 07:27:02 +0200

    created challenge 2

---
+++ Challenge -2/alex.js
@@ -0,0 +1,8 @@
+
+
+const firstname = 'Alex';
+const surname = 'Naidoo';
+const = 'Head of Marketing';
+
+const display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
