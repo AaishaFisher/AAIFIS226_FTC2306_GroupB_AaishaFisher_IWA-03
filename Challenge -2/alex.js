1bcfe319e29e9a42b9a502c8669f34071567fd21
Aai'sha Fisher <82815863+AaishaFisher@users.noreply.github.com>
2023-08-06 06:09:06 +0200

    Fixed challenge 1

--- Challenge -2/alex.js
+++ Challenge -2/alex.js
@@ -2,7 +2,7 @@
 
 const firstname = 'Alex';
 const surname = 'Naidoo';
-const = 'Head of Marketing';
+const role = 'Head of Marketing';
 
 const display= firstname + " " + surname + " (" + role + ")"
 document.querySelector('#alex').innerText = display
--- Challenge-1/script.js
+++ Challenge-1/script.js
@@ -1,5 +1,6 @@
-import { company } from './configuration';
-import { year } from  './configuration';
+import { company, year } from './configuration.js'
 
-const message = '© ' + company + ' (' + year + ')';
+
+const message = '© ' + company  +  year ; //the brackets was the issue. the string was incorrectly encased
+
 document.querySelector('footer').innerText = message;
