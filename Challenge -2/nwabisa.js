670acb8b029932f99694c8c92596e5001556ad3a
Aai'sha Fisher <82815863+AaishaFisher@users.noreply.github.com>
2023-08-07 04:51:15 +0200

    Updated tiny spelling changes

--- Challenge -2/nwabisa.js
+++ Challenge -2/nwabisa.js
@@ -1,9 +1,9 @@
   const nwabisa = { 
-     firstname: "Nwabisa",
-     surname: "Gabe",
+     firstName: "Nwabisa",
+     surName: "Gabe",
      role: "CEO"
   };
 
   export const role = nwabisa.role;
 
-document.querySelector('#nwabisa').innerText = nwabisa.firstname + " " + nwabisa.surname + " (" + nwabisa.role + ")";
+document.querySelector('#nwabisa').innerText = nwabisa.firstName + " " + nwabisa.surName + " (" + nwabisa.role + ")";
